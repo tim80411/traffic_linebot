1776cbd04ddc452df7333c575b7c25214fa40a68
Yiting Liao <tim80411@gmail.com>
2021-06-01 16:15:17 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 
 const PORT = 3000
 
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: 'main'
+}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('success')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
