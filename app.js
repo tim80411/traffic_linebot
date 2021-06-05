d2b301ea2cd5668469c0f5fb49fdede84912b948
Yiting Liao <tim80411@gmail.com>
2021-06-05 20:02:03 +0800

    feat: get data from trafiic API

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const axios = require('axios')
+const fs = require('fs')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+const { GetAuthorizationHeader } = require('./lib/auth')
 
 const PORT = process.env.PORT
 
@@ -16,6 +20,28 @@ app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/traffic', (req, res) => {
+  async function getData() {
+    try {
+      const data = await axios.get('https://traffic.transportdata.tw/MOTC/v2/Road/Traffic/Live/News/Freeway?$top=1&$format=JSON?$top=30&$format=JSON', {
+        headers: GetAuthorizationHeader()
+      })
+
+      fs.appendFile('./logs/res.txt', JSON.stringify(data.data) + '\n', err => {
+        if (err) console.error(err)
+      })
+
+      console.log(data.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  getData()
+
+  res.redirect('/')
 })
 
 app.listen(PORT, () => {
